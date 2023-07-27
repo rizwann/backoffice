716d79cd66675528e4aa52b0c1d00150014c7c94
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-07-28 00:32:40 +0200

    ADD: clerk authentication setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <p>Hello World</p>;
+import { UserButton } from "@clerk/nextjs";
+
+export default function Dashboard() {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
