9016ff2cb437a29bc60242012ef184dd2a0818e7
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-08-17 22:18:28 +0200

    ADD: orders table

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import { OrderColumn, columns } from "./columns";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const handleAddNew = () => {
+    router.push(`/${params.storeId}/orders/new`);
+  };
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="phone" />
+    </>
+  );
+};
