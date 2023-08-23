846ff28040e62234f6e922859c9815e120d49003
Rizwan Kabir <rizwankabirsizan@gmail.com>
2023-08-24 00:09:42 +0200

    ADD: dark theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { StoreSwitcher } from "@/components/store-switcher";
 import prismaDb from "@/lib/prismadb";
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { ModeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -21,6 +22,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4 ">
+          <ModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
